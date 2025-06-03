68dd179fb32dcbf8085e30c269bb5ae2ea5d55ad
Patty060315 <413730176@o365.tku.edu.tw>
2025-06-04 01:24:13 +0800

    Update magnet.js

--- magnet.js
+++ magnet.js
@@ -1,4 +1,11 @@
-let textMagnets = ["T", "K", "U", "E"];
+// 先產生原本的字卡
+let textMagnets = ["hello", "hiii", "sweet", "pattvira"];
+
+// 隨機產生5個A~Z的大寫英文字母
+for (let i = 0; i < 5; i++) {
+  let letter = String.fromCharCode(65 + floor(random(26)));
+  textMagnets.push(letter);
+}
 
 class Magnet {
   constructor() {
@@ -7,16 +14,16 @@ class Magnet {
     this.y = random(height);
     this.angle = random(TWO_PI);
     this.c = color(255);
-    
+
     this.bbox = font.textBounds(this.t, this.x, this.y, size);
-    this.pos = createVector(this.bbox.x, this.bbox.y);
+    this.pos = createVector(this.x, this.y); // 修正為隨機位置
     this.w = this.bbox.w;
     this.h = this.bbox.h;
-    
+
     this.fingerx = 0;
     this.fingery = 0;
   }
-  
+
   display() {
     push();
     translate(this.pos.x, this.pos.y);
@@ -27,23 +34,23 @@ class Magnet {
     fill(0);
     noStroke();
     textFont(font);
-    textSize(size/2);
+    textSize(size / 2);
     textAlign(CENTER, CENTER);
     text(this.t, 0, 0);
     pop();
-    
+
     fill(255, 0, 0);
     ellipse(this.fingerx, this.fingery, 10, 10);
   }
-  
+
   touch(thumbx, thumby, indexx, indexy) {
     let distBetweenFingers = dist(thumbx, thumby, indexx, indexy);
     this.fingerx = abs(thumbx - indexx) + min(thumbx, indexx);
     this.fingery = abs(thumby - indexy) + min(thumby, indexy);
-    
+
     let distFromFingers = dist(this.pos.x, this.pos.y, this.fingerx, this.fingery);
-    
-    if (distBetweenFingers < 40 && distFromFingers < this.w/2) {
+
+    if (distBetweenFingers < 40 && distFromFingers < this.w / 2) {
       this.c = color(255, 0, 0);
       this.pos.x = this.fingerx;
       this.pos.y = this.fingery;
