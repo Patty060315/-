8fde2099ec4ca80907500cde65706cdf90b5a26a
Patty060315 <413730176@o365.tku.edu.tw>
2025-06-04 02:07:01 +0800

    Update magnet.js

--- magnet.js
+++ magnet.js
@@ -1,13 +1,18 @@
-// 只產生指定的五個字卡，位置隨機但靠近畫面中心（±100px）
-let centerX = 400; // 畫面中心 x
-let centerY = 300; // 畫面中心 y
-let textMagnets = [
-  { t: "T", x: centerX + random(-100, 100), y: centerY + random(-100, 100) },
-  { t: "k", x: centerX + random(-100, 100), y: centerY + random(-100, 100) },
-  { t: "u", x: centerX + random(-100, 100), y: centerY + random(-100, 100) },
-  { t: "e", x: centerX + random(-100, 100), y: centerY + random(-100, 100) },
-  { t: "t", x: centerX + random(-100, 100), y: centerY + random(-100, 100) }
-];
+let textMagnets = [];
+
+function initTextMagnets() {
+  let centerX = 400;
+  let centerY = 300;
+  let chars = ['T', 'k', 'u', 'e', 't'];
+  textMagnets = [];
+  for (let i = 0; i < chars.length; i++) {
+    textMagnets.push({
+      t: chars[i],
+      x: centerX + random(-100, 100),
+      y: centerY + random(-100, 100)
+    });
+  }
+}
 
 let magnetIndex = 0;
 
